a5ca4e852bace7142caa6f1d04657d907171af6e
alzabirr <alzabirhussain@gmail.com>
2025-08-15 01:10:01 +0600

    ok

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ let h1 = document.querySelector("h1");
 h1.addEventListener("click",function(){
     h1.textContent = "fuck u"
     h1.style.color = "white"
+h1.addEventListener("dblclick", function(){
+    h1.textContent = "yousuf  magi"
+})
  
 })
